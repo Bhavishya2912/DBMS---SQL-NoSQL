e59f613dd315f391732783787ccdb6fedab4a12d
Bhavishya <69957211+Bhavishya2912@users.noreply.github.com>
2025-02-05 17:35:27 +0530

    Create Combine tables.sql

---
+++ SQL_Problems/leetcode/Combine tables.sql
@@ -0,0 +1,13 @@
+-- COMBINE TWO TABLES
+-- https://leetcode.com/problems/combine-two-tables/?envType=problem-list-v2&envId=m8baczxh
+
+
+-- Approach: 
+--   As the problem is to combine the first and second table, with main focus on the first table (persons) - as we need to show address for each person , and if address not present return NULL.
+--   Hence it's better to use left join with more focus on table (Persons) on Address.
+
+  
+SELECT p.firstName, p.lastName, a.city, a.state 
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId
