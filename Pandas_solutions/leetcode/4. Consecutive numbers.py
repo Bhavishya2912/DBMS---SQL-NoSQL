45a63e3f0dfb3932cd675d376bd27506df42ab2b
Bhavishya2912 <bhavishya2912@gmail.com>
2025-02-19 17:13:55 +0530

    consecutive numbers

--- Pandas_solutions/leetcode/4. Consecutive numbers.py
+++ Pandas_solutions/leetcode/4. Consecutive numbers.py
@@ -4,3 +4,19 @@
 # SQL SOLUTION: SQL_solutions\leetcode\4.Consecutive numbers.sql
 
 # PANDAS SOLUTION:
+
+import pandas as pd
+
+def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
+    num = list(logs['num'])
+
+    # set a set to store the consecutive numbers
+    ConsecutiveNums = []
+
+    for i in range (1, len(num)-1): 
+        if num[i-1] == num[i] and num[i+1] == num[i]: 
+            print(num[i])
+            ConsecutiveNums.append(num[i])
+
+    ConsecutiveNums = list(set(ConsecutiveNums))
+    return pd.DataFrame({'ConsecutiveNums': ConsecutiveNums})
