584d49f24fec0599602224adaef3152723ebf662
Bhavishya2912 <bhavishya2912@gmail.com>
2025-05-13 12:37:45 +0530

    32-2nd sol

--- SQL_solutions/leetcode/32. primary_dept_for_each_employee.sql
+++ SQL_solutions/leetcode/32. primary_dept_for_each_employee.sql
@@ -10,3 +10,10 @@ select employee_id, department_id
 from Employee
 group by employee_id
 having count(employee_id) = 1
+
+
+-- OR.. solution 2:
+SELECT employee_id, department_id
+FROM employee
+WHERE primary_flag = 'Y'
+or employee_id in (select employee_id from employee group by employee_id having count(employee_id) = 1)
